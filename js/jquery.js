5627ee01eaa7aac9c059388d9195a08754b51d69
Mohammed Rizvi <morizvi07@gmail.com>
2015-11-22 19:40:56 -0500

    Created line through event and submit event, however need to fix submit event so that only one title is allowed

--- js/jquery.js
+++ js/jquery.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
   //Alert is hidden
   $(".alert-danger").hide();
 
+  //Creating a list title
+   $(".btn-primary").on("click", function(e) {
+    e.preventDefault();
+    console.log("button works");
+
+    var listName = $("#listName").val();
+    var listTitle = $("<h3>");
+    var deleteBtn = $("<button>").addClass("btn btn-danger").append("Delete");
+    var deleteTitle = $("<h3>").append(deleteBtn);
+
+    listTitle.append(listName).append(deleteTitle);
+    $("th").append(listTitle);
+
+    $("#listName").val("").focus();
+  });
+
   //Clicking add button
   $(".btn-success").on("click", function(e) {
     e.preventDefault();
@@ -10,6 +26,10 @@ $(document).ready(function() {
 
     var newItem = $("#newItem").val().trim();
     console.log("new word");
+
+    if (newItem === "") {
+      return;
+    }
 
     //Function that shows alert box if input is duplicated
     var isDuplicate = false;
@@ -24,14 +44,16 @@ $(document).ready(function() {
       return;
     }
 
-    //Variables for new row and delete button
+    //Variables for new row, delete button, and check button
     var newRow = $("<tr>");
     var wordTd = $("<td>").addClass("word-td").append(newItem);
+    var completeBtn = $("<button>").addClass("btn btn-info").append("Completed");
+    var completeTd = $("<td>").append(completeBtn);
     var deleteBtn = $("<button>").addClass("btn btn-danger").append("Delete");
     var deleteTd = $("<td>").append(deleteBtn);
 
     //Appending new row and delete button to table body
-    newRow.append(wordTd).append(deleteTd);
+    newRow.append(wordTd).append(completeTd).append(deleteTd);
     $("tbody").append(newRow);
 
     //Clearing the input field
@@ -41,6 +63,11 @@ $(document).ready(function() {
   //Deleting added row
   $("table").on("click", ".btn-danger", function() {
     $(this).parent().parent().remove();
+  });
+
+  //Strike Through 
+  $("table").on("click", ".btn-info", function() {
+    $(this).parent().prev().css("text-decoration", "line-through");
   });
 
   //Clearing alert
